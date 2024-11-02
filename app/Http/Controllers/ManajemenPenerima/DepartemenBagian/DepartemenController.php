b97009825d128c67440b2b289f57929d50863b7a
Rizq Alwan Fauzan <r.alwanfauzan@gmail.com>
2024-11-02 15:03:30 +0700

    Membuat menu Departemen dengan fungsionalitas CRUD

---
+++ app/Http/Controllers/ManajemenPenerima/DepartemenBagian/DepartemenController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers\ManajemenPenerima\DepartemenBagian;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\ManajemenPenerima\DepartemenBagian\Departemen\StoreDepartemenRequest;
+use App\Http\Requests\ManajemenPenerima\DepartemenBagian\Departemen\UpdateDepartemenRequest;
+use App\Models\Departemen;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\View\View;
+
+class DepartemenController extends Controller
+{
+    public function index(): View
+    {
+        $departemen = Departemen::orderBy('updated_at', 'desc')->get();
+        $data       = [
+            'title'      => 'Departemen',
+            'departemen' => $departemen
+        ];
+
+        return view('pages.manajemen-penerima.departemen-bagian.departemen', $data);
+    }
+
+    public function show(Departemen $departemen): JsonResponse
+    {
+        return response()->json($departemen);
+    }
+
+    public function store(StoreDepartemenRequest $request): RedirectResponse
+    {
+        $validated = $request->validated();
+        Departemen::create($validated);
+        return redirect()->back()->with('success', 'Data departemen berhasil ditambahkan');
+    }
+
+    public function update(UpdateDepartemenRequest $request, Departemen $departemen): RedirectResponse
+    {
+        $validated = $request->validated();
+        $departemen->update($validated);
+        return redirect()->back()->with('success', 'Data departemen berhasil diperbarui');
+    }
+
+    public function destroy(Departemen $departemen): RedirectResponse
+    {
+        $departemen->delete();
+        return redirect()->back()->with('success', 'Data departemen berhasil dihapus');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\ManajemenPenerima\DepartemenBagian\DepartemenController;
 use Illuminate\Support\Facades\Route;
 
 // Route::get('/', function () {
@@ -8,3 +9,13 @@ use Illuminate\Support\Facades\Route;
 // });
 
 Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
+
+Route::prefix('manajemen-penerima')->name('manajemen-penerima.')->group(function () {
+    Route::prefix('departemen-bagian')->name('departemen-bagian.')->group(function () {
+        Route::get('/departemen', [DepartemenController::class, 'index'])->name('departemen');
+        Route::get('/departemen/{departemen}', [DepartemenController::class, 'show'])->name('departemen.show');
+        Route::post('/departemen', [DepartemenController::class, 'store'])->name('departemen.store');
+        Route::put('/departemen/{departemen}', [DepartemenController::class, 'update'])->name('departemen.update');
+        Route::delete('/departemen/{departemen}', [DepartemenController::class, 'destroy'])->name('departemen.destroy');
+    });
+});
